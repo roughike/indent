e2b7583471e45e085c3d6cc5d48ce766846ec7bf
Iiro Krankka <iiro.krankka@gmail.com>
2020-02-10 18:46:13 +0100

    Add .travis.yml & coveralls

---
+++ tool/coveralls.sh
@@ -0,0 +1,22 @@
+# Install dart_coveralls; gather and send coverage data.
+if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "stable" ]; then
+  echo "Running coverage..."
+  pub global activate dart_coveralls
+  dart dart_coveralls report \
+    --retry 2 \
+    --exclude-test-files \
+    --throw-on-error \
+    --throw-on-connectivity-error \
+    --debug \
+    test/_indent_test.dart
+  echo "Coverage complete."
+else
+  if [ -z ${COVERALLS_TOKEN+x} ]; then echo "COVERALLS_TOKEN is unset"; fi
+  if [ -z ${TRAVIS_DART_VERSION+x} ]; then
+    echo "TRAVIS_DART_VERSION is unset";
+  else
+    echo "TRAVIS_DART_VERSION is $TRAVIS_DART_VERSION";
+  fi
+
+  echo "Skipping coverage for this configuration."
+fi
